34956933f3d0cfeb536e4cd37ce0df74b86d4ee1
John Brinker <john@Johns-MacBook-Pro.local>
2018-08-02 13:40:08 -0400

    created initial Book class

--- src/com/libraryterminal/Book.java
+++ src/com/libraryterminal/Book.java
@@ -1,5 +1,89 @@
 package com.libraryterminal;
 
 public class Book {
-
+	private String title;
+	private String authorLast;
+	private String authorFirst;
+	private boolean status;
+	private String dueDate;
+	
+	
+	
+	public Book(String title, String authorLast, String authorFirst, boolean status, String dueDate) {
+		super();
+		this.title = title;
+		this.authorLast = authorLast;
+		this.authorFirst = authorFirst;
+		this.status = status;
+		this.dueDate = dueDate;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the authorLast
+	 */
+	public String getAuthorLast() {
+		return authorLast;
+	}
+	/**
+	 * @param authorLast the authorLast to set
+	 */
+	public void setAuthorLast(String authorLast) {
+		this.authorLast = authorLast;
+	}
+	/**
+	 * @return the authorFirst
+	 */
+	public String getAuthorFirst() {
+		return authorFirst;
+	}
+	/**
+	 * @param authorFirst the authorFirst to set
+	 */
+	public void setAuthorFirst(String authorFirst) {
+		this.authorFirst = authorFirst;
+	}
+	/**
+	 * @return the status
+	 */
+	public boolean isStatus() {
+		return status;
+	}
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	/**
+	 * @return the dueDate
+	 */
+	public String getDueDate() {
+		return dueDate;
+	}
+	/**
+	 * @param dueDate the dueDate to set
+	 */
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	
+	@Override
+	public String toString() {
+		return title + "|" + authorLast + "," + authorFirst + "|" + status + "|" + dueDate;
+	}
+	
 }
